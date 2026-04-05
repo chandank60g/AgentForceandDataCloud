e271027ab759d827468da5a5d437fe52665223df
chandank60g <chandansensibull@gmail.com>
2026-04-05 17:03:01 +0530

    javascript

--- force-app/main/default/lwc/testLWC/testLWC.js
+++ force-app/main/default/lwc/testLWC/testLWC.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class TestLWC extends LightningElement {}
+export default class TestLWC extends LightningElement {
+
+    // // write code below to check if the 2 number is graeter and diplay on ui
+    num1 = 10;  
+    num2 = 5;
+    get greaterNumber() {
+        return this.num1 > this.num2 ? this.num1 : this.num2;
+    }   
+    
+}
